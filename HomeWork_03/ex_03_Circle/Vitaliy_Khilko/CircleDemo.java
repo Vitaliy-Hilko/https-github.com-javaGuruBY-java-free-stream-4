58ee4a7a804e4b28549f6dd08ecbca235e4d33e9
Vitaliy Khilko <62650726+Vitaliy-Hilko@users.noreply.github.com>
2020-04-06 16:22:46 +0200

    Ex_03_CircleDemo
    My circledemo

---
+++ HomeWork_03/ex_03_Circle/Vitaliy_Khilko/CircleDemo.java
@@ -0,0 +1,8 @@
+package CircleDemo;
+
+public class CircleDemo {
+    public static void main(String[] args) {
+        Circle findAreaCircle = new Circle(55);
+        findAreaCircle.calculateArea(findAreaCircle.radius);
+    }
+}
