2b752bdf03bfcd870b1107c812a13bc743173805
Vitaliy Khilko <62650726+Vitaliy-Hilko@users.noreply.github.com>
2020-04-10 20:59:50 +0200

    Delete EncoderDemo.java

--- HomeWork_03/ex_04_Encoder/Vitaliy_Khilko/EncoderDemo.java
+++
@@ -1,8 +0,0 @@
-public class EncoderDemo {
-    public static void main(String[] args) {
-        Encoder encoder = new Encoder();
-
-        encoder.encode((short) 55);
-        encoder.decode('P');
-    }
-}
