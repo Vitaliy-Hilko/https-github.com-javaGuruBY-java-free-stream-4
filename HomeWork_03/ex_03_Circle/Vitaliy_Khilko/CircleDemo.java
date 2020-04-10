9627780684ea270c6c22f76069085cb35040ba92
Vitaliy Khilko <62650726+Vitaliy-Hilko@users.noreply.github.com>
2020-04-10 20:59:30 +0200

    Delete CircleDemo.java

--- HomeWork_03/ex_03_Circle/Vitaliy_Khilko/CircleDemo.java
+++
@@ -1,8 +0,0 @@
-package CircleDemo;
-
-public class CircleDemo {
-    public static void main(String[] args) {
-        Circle findAreaCircle = new Circle(55);
-        findAreaCircle.calculateArea(findAreaCircle.radius);
-    }
-}
