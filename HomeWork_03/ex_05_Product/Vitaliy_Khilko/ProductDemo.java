14760cb8b5e042f7b55dc1a12b78f30859be527a
Vitaliy-Hilko <vitaminz.47@yandex.ru>
2020-04-10 19:40:01 +0300

    My ProductDemo bean, service.

--- HomeWork_03/ex_05_Product/Vitaliy_Khilko/ProductDemo.java
+++ HomeWork_03/ex_05_Product/Vitaliy_Khilko/ProductDemo.java
@@ -1,13 +1,10 @@
+import bean.Product;
+import service.ServiceProduct;
+
 public class ProductDemo {
     public static void main(String[] args) {
-        Product name = new Product("Bread");
-        System.out.println(name.product);
-        name.setRegularPrice(28.34);
-        System.out.println(name.getRegularPrice());
-        name.setDiscount(3);
-        System.out.println(name.getDiscount());
-        name.actualPrice();
-        name.printInformation();
-
+        Product name = new Product();
+        ServiceProduct service = new ServiceProduct();
+        service.printInformation(name);
     }
 }
