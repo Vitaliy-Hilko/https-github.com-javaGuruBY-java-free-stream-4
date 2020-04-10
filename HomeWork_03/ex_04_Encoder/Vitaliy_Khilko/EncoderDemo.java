f42f6824cf17eb6dc1a872f9866bbb7869a8a6db
Vitaliy-Hilko <vitaminz.47@yandex.ru>
2020-04-10 18:41:32 +0300

    My EncoderDemo bean, service.

--- HomeWork_03/ex_04_Encoder/Vitaliy_Khilko/EncoderDemo.java
+++ HomeWork_03/ex_04_Encoder/Vitaliy_Khilko/EncoderDemo.java
@@ -1,8 +1,15 @@
+import bean.Encoder;
+import service.ServiceEncoder;
+
 public class EncoderDemo {
     public static void main(String[] args) {
         Encoder encoder = new Encoder();
+        ServiceEncoder service = new ServiceEncoder();
 
-        encoder.encode((short) 55);
-        encoder.decode('P');
+        service.encode(encoder);
+        service.decode(encoder);
+
+        service.encode((short)23);
+        service.decode('P');
     }
 }
